d94757741954b1e52f405049b04e7e4005813d77
Han Han <haannn@qq.com>
2020-05-23 02:59:17 +0800

    update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,31 +5,17 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-ci-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ci-demo');
-  });
-
-  it('should render title', () => {
+  it('should have title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-ci-demo app is running!');
+    expect(compiled.querySelector('p').textContent).toContain(
+      'angular ci demo'
+    );
   });
 });
